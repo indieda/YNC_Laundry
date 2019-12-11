a2d8b3ce873ed5a4cf9f53dd7b8e416849f23447
indieda <yun_da_legacy@hotmail.com>
2019-12-11 18:36:29 +0800

    Add files via upload
    updates "waiting" every 30 seconds. For 1 arduino only.

--- arduino BLE.py
+++ arduino BLE.py
@@ -7,13 +7,12 @@ class MyDel(btle.DefaultDelegate):
     def handleNotification(self,cHandle,data):
         print("Handling notification...")
         print(data.decode('utf8'))
-        
 print("Connecting...")
 p = btle.Peripheral("50:51:a9:fd:a6:f1")
 p.setDelegate(MyDel(0))
 
 while True:
-    if p.waitForNotifications(1.0):
+    if p.waitForNotifications(30.0):
         continue
     print("Waiting")
 
