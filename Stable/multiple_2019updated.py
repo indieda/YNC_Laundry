1d99fc2a91674744035ea1419cbdd4da1fc3ffc2
indieda <yun_da_legacy@hotmail.com>
2019-12-14 20:41:26 +0800

    Update multiple_2019updated.py

--- Stable/multiple_2019updated.py
+++ Stable/multiple_2019updated.py
@@ -1,5 +1,6 @@
 #Taken from this site: https://raspberrypi.stackexchange.com/questions/96247/are-there-reliable-methods-for-connecting-reconnecting-to-multiple-bluetooth-le
 #The person added the sleep function.
+#hrm
 
 from bluepy import btle
 import struct, os
