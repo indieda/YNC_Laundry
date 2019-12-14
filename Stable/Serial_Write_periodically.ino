5d3cf6a410b9f2f09d3f92862b9fb0fca2867f1b
indieda <yun_da_legacy@outlook.com>
2019-12-14 19:06:38 +0800

    restrutured and added stable

---
+++ Stable/Serial_Write_periodically.ino
@@ -0,0 +1,14 @@
+#include "LowPower.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write("BLE2");
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
+}
