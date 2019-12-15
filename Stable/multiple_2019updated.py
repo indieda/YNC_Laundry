57e020e6beec70e2fd3ddedfc071ae7e92d7e3be
indieda <yun_da_legacy@outlook.com>
2019-12-16 01:38:08 +0800

    timing

--- Stable/multiple_2019updated.py
+++ Stable/multiple_2019updated.py
@@ -72,7 +72,7 @@ def perif_wait(perif):
         try:
             perif.disconnect()
             #This sleep value needs to change when more POSTS to https are done. For 2 devices, 0.4 is still okay.
-            time.sleep(0.4)
+            time.sleep(0.1)
         except Exception as e:
             print('failed to disconnect!', e)
             pass
