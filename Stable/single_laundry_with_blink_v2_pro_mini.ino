4a7a2ebcfb71cd0b7797877ba97b66d981128617
indieda <yun_da_legacy@outlook.com>
2020-01-18 01:52:20 +0800

    Stable code, sleep runs at 56microamps

--- Stable/single_laundry_with_blink_v2_pro_mini.ino
+++ Stable/single_laundry_with_blink_v2_pro_mini.ino
@@ -1,5 +1,6 @@
 //12 Dec 2019
 //Code written mostly by Yun Da, with reference to arduino ref page and sparksfun.
+//BLE code was also referenced from https://makersportal.com/blog/2018/3/21/arduino-internet-of-things-part-2-lowering-the-power-consumption-of-the-atmega328p-breadboard-arduino-with-the-hm-10-bluetooth-module
 
 #include <LowPower.h>
 #include <SoftwareSerial.h>
@@ -16,7 +17,6 @@ const int ldrGND = 12;
 SoftwareSerial ble(8,9);
 
 int lightCal, lightVal, dk, lt, dk_num, lt_num, iter, darkval;
-unsigned message;
 
 //ensure you start the device with the photodiode pointing at the LED when dark, not when bright.
 const int light_threshold = 150;
@@ -24,8 +24,8 @@ const int light_threshold = 150;
 
 //Things that you can update:
 int scan_blink_iter = 20;
-unsigned tml = SLEEP_1S;
-unsigned tmd = SLEEP_1S;
+unsigned tml = SLEEP_8S;
+unsigned tmd = SLEEP_8S;
 //control how long between each blinking check
 unsigned tmib = SLEEP_250MS;
 
@@ -57,22 +57,18 @@ bool isblink(int scan_blink_iter, int dk_num, int lt_num, int iter)
     return 0;
   }
   else {
+  //turn on the ble again.
+    pinMode(ble_gnd,OUTPUT);
+    digitalWrite(ble_gnd,LOW);
+    delay(1000);
     Serial.write("b1");
+    Serial.flush();
+    ble.write("b1");
+    ble.flush();
+    pinMode(ble_gnd,INPUT);
     return 1;
   }
 }
-
-
-
-
-
-unsigned bluetooth_init(char message) {
- Serial.write(message);
-}
-
-
-
-
 
 //Sleep functions (no need to change)
 void sleepl()
@@ -87,16 +83,12 @@ LowPower.powerDown(tmd, ADC_OFF, BOD_OFF);
 }
 void sleep_isblink()
 {
-//LowPower.powerDown(tmib, ADC_OFF, BOD_OFF);
-  LowPower.idle(tmib, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
+LowPower.powerDown(tmib, ADC_OFF, BOD_OFF);
+//  LowPower.idle(tmib, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
 }
 
 void setup()
 {
-  // We'll set up the LED pin to be an output.
-  //9600 is the baud rate of the BLE.
-  Serial.begin(9600);
-  //  pinMode(ledPin, OUTPUT);
   lightCal = analogRead(sensorPin);
   //we will take a single reading from the light sensor and store it in the lightCal
   //variable. This will give us a prelinary value to compare against in the loop
@@ -105,11 +97,11 @@ void setup()
   pinMode(ldrGND,OUTPUT);
 
   pinMode(ble_vcc,OUTPUT);
-  pinMode(ble_gnd,OUTPUT);
+  pinMode(ble_gnd,INPUT);
   digitalWrite(ble_vcc,HIGH);
-  digitalWrite(ble_gnd,LOW);
-
-
+  //9600 is the baud rate of the BLE.
+  Serial.begin(9600);
+  ble.begin(9600);
 }
 
 //START of main loop.
@@ -126,24 +118,40 @@ void loop()
   }
   lightVal = analogRead(sensorPin);
   Serial.println(lightVal);
+  ble.write(lightVal);
+  ble.flush();
   // Check if it is dark.
   if (lightVal < light_threshold)
-  {
+  {    
+  //turn on the ble again.
+    pinMode(ble_gnd,OUTPUT);
+    digitalWrite(ble_gnd,LOW);
+    delay(1000);
     Serial.write("d1");
     //This is an extremely important statement to ensure that all the bytes are sent over bluetooth before entering sleep mode. Without it, you wouldn't be able to decode the messages properly. Read for more: https://arduino.stackexchange.com/questions/14411/low-power-library-messing-up-serial-text 
     Serial.flush();
+    ble.write("d1");
+    ble.flush();
     delay(2000);
     pinMode(ldrOUT,INPUT); //Turn off LDR
     pinMode(ldrGND,INPUT); //Turn off LDR
+    pinMode(ble_gnd,INPUT); // Turn off the ble
     sleepl(); sleepl(); sleepl(); sleepl(); sleepl(); sleepl(); sleepl();
   }
   else
   {
+  //turn on the ble again.
+    pinMode(ble_gnd,OUTPUT);
+    digitalWrite(ble_gnd,LOW);
+    delay(1000);
     Serial.write("l1");
     Serial.flush();
+    ble.write("l1");
+    ble.flush();
     delay(2000);
     pinMode(ldrOUT,INPUT); //Turn off LDR
     pinMode(ldrGND,INPUT); //Turn off LDR
+    pinMode(ble_gnd,INPUT); // Turn off the ble
     sleepl(); sleepl();
   }
 }
