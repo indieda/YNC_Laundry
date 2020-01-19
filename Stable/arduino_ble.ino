84af420ac2103385ea8ea1c596512aaac66c87a2
indieda <yun_da_legacy@hotmail.com>
2020-01-20 03:00:30 +0800

    Changed timings

--- Stable/arduino_ble.ino
+++ Stable/arduino_ble.ino
@@ -1,143 +1,158 @@
-#include <SoftwareSerial.h>
+//20 Jan 2020
+//Code written mostly by Yun Da, with reference to arduino ref page and sparksfun.
+//BLE code was also referenced from https://makersportal.com/blog/2018/3/21/arduino-internet-of-things-part-2-lowering-the-power-consumption-of-the-atmega328p-breadboard-arduino-with-the-hm-10-bluetooth-module
+
 #include <LowPower.h>
+#include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(7, 8); // RX, TX
-int vcc = 5;
-int gnd = 4;
-void setup()
- {
-     mySerial.begin(9600);
-     pinMode(vcc,OUTPUT);
-     pinMode(gnd,OUTPUT);
-     digitalWrite(5,HIGH);
-     digitalWrite(gnd,LOW);
-     Serial.begin(9600);
- }
+int ble_rxd = 9; // ble transfer out
+int ble_txd = 8; // ble receiver
+int ble_vcc = 7; //Power in
+int ble_gnd = 6; // Ground
 
-void loop()
- {
-Serial.write("looped");
-pinMode(vcc,OUTPUT);
-pinMode(gnd,OUTPUT);
-digitalWrite(vcc,HIGH);
-digitalWrite(gnd,LOW);
-delay(500);
+//global variables (by hardware design)
+const int sensorPin = 0;
+const int ldrOUT = 11;
+const int ldrGND = 12;
+SoftwareSerial ble(9, 8); //RXD,TXD
 
-//delay(2000);
-/*
-if (mySerial.available() >0) // This runs once only, on the first connect. To print out onto serial monitor whatever I'm receving from BLE. 
+int lightCal, lightVal, dk, lt, dk_num, lt_num, iter, darkval;
 
+//ensure you start the device with the photodiode pointing at the LED when dark, not when bright.
+const int light_threshold = 150;
+
+//Things that you can update:
+int scan_blink_iter = 12;
+unsigned tml = SLEEP_8S;
+unsigned tmd = SLEEP_8S;
+//control how long between each blinking check
+unsigned tmib = SLEEP_250MS;
+
+int blinking_differential_thresh = 6;
+
+//General functions to be used:
+//1- check if light is blinking
+//We create a function/method that does a search for 5 seconds, checking if the LED is blinking, or in a steady state.
+
+bool isblink(int scan_blink_iter, int dk_num, int lt_num, int iter)
 {
-while (mySerial.available()>0) //softwareserial's read is char by char. While make sure everything is read.
-{
-  Serial.write(mySerial.read()); 
-}
-Serial.write("Listening"); // delete after deploy
-
-mySerial.write("light");
-mySerial.flush();
-delay(3000);
-
-for (int i = 0; i < 4; i++) {
-mySerial.write("hello");
-mySerial.flush();
-mySerial.write(i);
-mySerial.flush();
-Serial.write("printed hello ");
-Serial.print(i);
-Serial.flush();
-delay(1000);
-}
-
-
-
-pinMode(vcc,INPUT);
-pinMode(gnd,INPUT);
-delay(1000);
-}
-
-*/
-
-
-/*
-//To delete
-for (int i = 0; i < 4; i++) {
-mySerial.write("hello\n");
-mySerial.flush();
-mySerial.write(i);
-mySerial.flush();
-Serial.write("printed hello ");
-Serial.print(i);
-Serial.flush();
-delay(1000);
-}
-*/
-
-mySerial.write("on");
-mySerial.flush();
-Serial.write("printed hello ");
-Serial.flush();
-Serial.flush();
-delay(3000);
-
-//Part 2 Checks if the BLE module is connected.
-/*
-if (mySerial.isListening())
-{
-  Serial.write("Listening");
-  mySerial.write("hello");
-mySerial.flush();
-pinMode(vcc,OUTPUT);
-pinMode(gnd,OUTPUT);
-digitalWrite(vcc,HIGH);
-digitalWrite(gnd,LOW);
-delay(1000);
-continue; 
+  for (dk_num, lt_num, iter; iter < scan_blink_iter; iter++) {
+    lightVal = analogRead(sensorPin);
+    if (lightVal < light_threshold) {
+      dk_num++;
+      sleep_isblink();
+    }
+    else if (lightVal >= light_threshold) {
+      lt_num++;
+      sleep_isblink();
+    }
   }
-*/
-
-
-//Part 3 Checks if there's anything being received on subsequent loops after the first one where "connected" is printed.
-/*
-while (mySerial.available()>0)
-{
-  Serial.write(mySerial.read());
+  //Possibility to add an exponential function here, that scales to a higher threshold as the number of cycles increase.
+  if ((dk_num - lt_num) > blinking_differential_thresh) {
+    return 0;
+  }
+  else if ((dk_num - lt_num) < -blinking_differential_thresh) {
+    return 0;
+  }
+  else {
+    //turn_on_ble;
+    pinMode(ble_vcc, OUTPUT);
+    pinMode(ble_gnd, OUTPUT);
+    digitalWrite(ble_vcc, HIGH);
+    digitalWrite(ble_gnd, LOW);
+    delay(1388);
+    ble.write("error");
+    ble.flush();
+    delay(3000);
+    pinMode(ble_vcc, INPUT); //turn off ble
+    pinMode(ble_gnd, INPUT);
+    return 1;
+  }
 }
-Serial.write("Nothing in mySerial");
-*/
 
-//Part 4
+//Sleep functions (no need to change)
+void sleepl()
+{
+  //LowPower.idle(tml, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+  LowPower.powerDown(tml, ADC_OFF, BOD_OFF);
+}
+void sleepd()
+{
+  //  LowPower.idle(tmd, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+  LowPower.powerDown(tmd, ADC_OFF, BOD_OFF);
+}
+void sleep_isblink()
+{
+  LowPower.powerDown(tmib, ADC_OFF, BOD_OFF);
+  //  LowPower.idle(tmib, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
+}
 
-pinMode(vcc,INPUT);
-pinMode(gnd,INPUT);
-LowPower.powerDown(SLEEP_4S,ADC_OFF,BOD_OFF);
+void setup()
+{
+  ble.begin(9600);//9600 is the baud rate of the BLE.
+  lightCal = analogRead(sensorPin);
+  //we will take a single reading from the light sensor and store it in the lightCal
+  //variable. This will give us a prelinary value to compare against in the loop
+  //darkval = analogRead(sensorPin);
+  pinMode(ldrOUT, OUTPUT);
+  pinMode(ldrGND, OUTPUT);
 
+  pinMode(ble_vcc, OUTPUT); // Turn on BLE
+  pinMode(ble_gnd, OUTPUT);
+  digitalWrite(ble_vcc, HIGH);
+  digitalWrite(ble_gnd, LOW); //Turn on BLE
+  //Serial.begin(9600); 
+}
 
-  /*
-     while (Serial.available() > 0)  
-     {
-         comdata = Serial.readString();
-         delay(15);
-         Serial.print(comdata);
-         Serial.println("");
-         mySerial.print(Serial.readString());
-     }
+//START of main loop.
+void loop()
+{
+  //Turn on the photoresistor components.
+  pinMode(ldrOUT, OUTPUT);
+  pinMode(ldrGND, OUTPUT);
+  digitalWrite(ldrOUT, HIGH);
+  digitalWrite(ldrGND, LOW);
 
-while (Serial.available() > 0)  
-     {
-         comdata = Serial.readString();
-         delay(15);
-         Serial.print(comdata);
-         Serial.println("");
-     }
-     
-     while (mySerial.available() > 0)
-     {
-      comdata = mySerial.readString();
-         delay(15);
-         Serial.print(comdata);
-         Serial.println("");
-     }
-     delay(1000);
- */
- }
+  //Take a reading using analogRead() on sensor pin and store it in lightVal
+  while (isblink(scan_blink_iter, 0, 0, 0)) {
+    continue;
+  }
+  //lightVal = analogRead(sensorPin);
+  //Serial.println(lightVal);
+
+  if (lightVal < light_threshold) // Check if it is dark.
+  {
+    //turn_on_ble;
+    pinMode(ble_vcc, OUTPUT);
+    pinMode(ble_gnd, OUTPUT);
+    digitalWrite(ble_vcc, HIGH);
+    digitalWrite(ble_gnd, LOW);
+    delay(1300);
+
+    ble.write("off");
+    ble.flush();     //This is an extremely important statement to ensure that all the bytes are sent over bluetooth before entering sleep mode. Without it, you wouldn't be able to decode the messages properly. Read for more: https://arduino.stackexchange.com/questions/14411/low-power-library-messing-up-serial-text
+    pinMode(ldrOUT, INPUT); //Turn off LDR
+    pinMode(ldrGND, INPUT); //Turn off LDR
+    delay(3000);
+    pinMode(ble_vcc,INPUT); //Turn off BLE
+    pinMode(ble_gnd,INPUT);
+    sleepd(); sleepd(); sleepd(); sleepd(); sleepd(); sleepd(); sleepd();sleepd();
+  }
+  else
+  {
+    //turn_on_ble;
+    pinMode(ble_vcc, OUTPUT);
+    pinMode(ble_gnd, OUTPUT);
+    digitalWrite(ble_vcc, HIGH);
+    digitalWrite(ble_gnd, LOW);
+    delay(1388);
+    ble.write("on");
+    ble.flush();
+    pinMode(ldrOUT, INPUT); //Turn off LDR
+    pinMode(ldrGND, INPUT); //Turn off LDR
+    delay(3000);
+    pinMode(ble_vcc,INPUT);//turn_off_ble;
+    pinMode(ble_gnd,INPUT);//turn_off_ble;
+    sleepl(); sleepl(); sleepl(); sleepl();
+  }
+}
