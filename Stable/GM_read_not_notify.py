594ced651aaf68fb989442f96a86cf55340cc987
indieda <yun_da_legacy@hotmail.com>
2020-01-20 02:03:38 +0800

    lengthened timeout period

--- Stable/GM_read_not_notify.py
+++ Stable/GM_read_not_notify.py
@@ -83,7 +83,7 @@ while True:
 try:
         for addr_i in cendana_addr:
             try:
-                func_timeout(0.9,read_ble,args=(addr_i,i))
+                func_timeout(2.2,read_ble,args=(addr_i,i))
                 washer_state_array[i]=read_ble.data_decode
             #upload = threading.Thread(target=stdhandle, args = (read_ble.data_decode,i), daemon = True)
             except Exception as e:
@@ -105,5 +105,6 @@ try:
                 time_prev = time.time()
             i = i+1
             sleep(1.0)
-    except:
+    except Exception as e:
+        print(e)
         pass
