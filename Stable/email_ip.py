45b9f4f1f975820f1d0bcfa7cb0e5de28e3bdce6
indieda <yun_da_legacy@hotmail.com>
2020-01-31 23:49:34 +0800

    Update email_ip.py

--- Stable/email_ip.py
+++ Stable/email_ip.py
@@ -2,11 +2,12 @@ from urllib.request import urlopen
 import re
 import smtplib
 import sm
+import socket
 
 # Setup our login credentials
 from_address = 'HalcyonLaundry@gmail.com'
 to_address = 'HalcyonLaundry@gmail.com'
-subject = 'Pi IP'
+subject = 'HalcyonLaundry Cendana IP'
 username = 'HalcyonLaundry@gmail.com'
 password = 'Yundachua1'
 
@@ -19,11 +20,16 @@ request = urlopen(url).read().decode('utf-8')
 # We extract the IP address only
 ourIP = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", request)
 ourIP = str(ourIP)
-print ("Our IP address is: ", ourIP)
+internalIP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+internalIP.connect(("8.8.8.8",80))
+ip_to_send = str(internalIP.getsockname()[0])
+print("The internal ip is:", ip_to_send)
+internalIP.close()
+print ("Our public IP address is: ", ourIP)
 
-def send_email(ourIP):
+def send_email(ip_to_send):
 # Body of the email
-    body_text = ourIP + ' is our YNC_Laundry IP address'
+    body_text =  ip_to_send + ' is our YNC_Laundry IP address' + ourIP + "is our public external ip"
     msg = '\r\n'.join(['To: %s' % to_address, 'From: %s' % from_address, 'Subject: %s' % subject, '', body_text])
 
     # Actually send the email!
@@ -39,11 +45,11 @@ with open('/home/pi/Documents/last_ip.txt', 'rt') as last_ip:
     last_ip = last_ip.read() # Read the text file
 
 # Check to see if our IP address has really changed
-if last_ip == ourIP:
+if last_ip == ourIP: #this will always send out. Deliberately not made it last_ip = ip_to_send so that it will always send!
     print("Our IP address has not changed.")
 else:
     print ("We have a new IP address.")
     with open('/home/pi/Documents/last_ip.txt', 'wt') as last_ip:
-        last_ip.write(ourIP)
+        last_ip.write(ip_to_send)
     print ("We have written the new IP address to the text file.")
-    send_email(ourIP)
+    send_email(ip_to_send)
